fda191e63d9e11aaf51682adf3865ed72c2d8d1d
David Forero <davidforero717@gmail.com>
2021-08-10 14:59:16 -0500

    se realiza la creacion de gastos

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expense;
+use App\Models\ExpenseReport;
 use Illuminate\Http\Request;
 
 class ExpenseController extends Controller
@@ -13,7 +15,7 @@ class ExpenseController extends Controller
      */
     public function index()
     {
-        //
+
     }
 
     /**
@@ -21,9 +23,11 @@ class ExpenseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(ExpenseReport $expenseReport)
     {
-        //
+        return view('expense.create',[
+            'report' => $expenseReport 
+        ]);
     }
 
     /**
@@ -32,9 +36,15 @@ class ExpenseController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, ExpenseReport $expenseReport)
     {
-        //
+      $expense = new Expense();
+      $expense->description = $request->get('description');
+      $expense->amount = $request->get('amount');
+      $expense->expense_report_id = $expenseReport->id;
+      $expense->save();
+
+      return redirect('expense_reports/' . $expenseReport->id);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExpenseReportController;
+use App\Http\Controllers\ExpenseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,7 @@ Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::resource('/expense_reports', ExpenseReportController::class);
 
 Route::get('/expense_reports/{id}/confirmDelete', [ExpenseReportController::class, 'confirmDelete']);
+
+Route::get('/expense_reports/{expense_report}/expenses/create', [ExpenseController::class, 'create']);
+
+Route::post('/expense_reports/{expense_report}/expenses', [ExpenseController::class, 'store']);
