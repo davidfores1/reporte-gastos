374f949268d5e3a9bd7e4d13b04b59283793ff1a
David Forero <davidforero717@gmail.com>
2021-08-10 07:10:59 -0500

    se realiza un formato base para las extencion de blade

--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -1,14 +1,15 @@
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h1>Reports</h1>
-            <table class="table">
-                @foreach($expenseReports as $expenseReport)
-                <tr>
-                    <td>{{ $expenseReport->title }}</td>
-                </tr>
-                @endforeach
-            </table>
-        </div>
-    </div>
-</div>
+   @extends('layouts.base')
+   @section('content')
+   <div class="row">
+       <div class="col">
+           <h1>Reports</h1>
+           <table class="table">
+               @foreach($expenseReports as $expenseReport)
+               <tr>
+                   <td>{{ $expenseReport->title }}</td>
+               </tr>
+               @endforeach
+           </table>
+       </div>
+   </div>
+  @endsection 
