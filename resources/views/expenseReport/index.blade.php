4bb08bbc73b8b8e3961bfd99d76f2cf3d379864f
David Forero <davidforero717@gmail.com>
2021-08-10 07:03:22 -0500

    se realiza por medio del controlador y el modelo llamar datos de la base de datos y retornar a la vista, por medio de un @foreach pordelo iterarlo

---
+++ resources/views/expenseReport/index.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Reports</h1>
+            <table class="table">
+                @foreach($expenseReports as $expenseReport)
+                <tr>
+                    <td>{{ $expenseReport->title }}</td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExpenseReportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,5 @@ use App\Http\Controllers\DashboardController;
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::resource('/expense_reports', ExpenseReportController::class);
