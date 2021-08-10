d55a4a5703fa977b46e60b1bff71061ae4aeca35
David Forero <davidforero717@gmail.com>
2021-08-10 09:23:55 -0500

    se realiza la creacion del reporte

--- resources/views/expenseReport/create.blade.php
+++ resources/views/expenseReport/create.blade.php
@@ -13,7 +13,8 @@
 
    <div class="row">
        <div class="col">
-           <form action="/expense_repots" method="POST">
+           <form action="/expense_reports" method="POST">
+               @csrf
                <div class="form-group">
                    <label for="title">Title:</label>
                    <input type="text" class="form-control" id="title" name="title" placeholder="Type a title">
